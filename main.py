2590d4c80454e4106adc0106cd366faf503ee14a
vacobb <vacobb07@gmail.com>
2025-07-02 22:12:15 -0400

    created print_slot_machine function

--- main.py
+++ main.py
@@ -35,6 +35,15 @@ def get_slot_machine_spin(rows, cols, symbols):
     return columns
 
 
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
