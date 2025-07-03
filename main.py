e47ee27f0a1a83756d57e95ebcb5a66eddf64138
vacobb <vacobb07@gmail.com>
2025-07-02 21:53:32 -0400

    created check to make sure user has enough in balance for bet

--- main.py
+++ main.py
@@ -51,8 +51,15 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount. Your current balance is: ${balance}.")
+        else:
+            break
+            
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}.")
 
 
