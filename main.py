77d0ee57b5140de2aca569f96313b3040f65f6ee
vacobb <vacobb07@gmail.com>
2025-07-02 21:49:47 -0400

    created bet function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 
 def deposit():
@@ -30,9 +32,28 @@ def get_number_of_lines():
         
     return lines
 
+
+def get_bet():
+    while True:
+        amount = input("What wouuld you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    
+    return amount
+
+
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance, lines)
+    bet = get_bet()
+    total_bet = bet * lines
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}.")
+
 
 main()
